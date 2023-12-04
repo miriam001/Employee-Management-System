18955fca3d3062932df4870a622430b5026cbe57
FarzeenKist <farzeenkistnareddy@gmail.com>
2023-12-04 13:17:11 +0400

    Revert "remove unused files, some improvements and bug fixes"
    This reverts commit f1165cd84ce0fbc5c096f1eb66ac35c5f506b1c4.

---
+++ .dfx/local/canisters/canister_azle_app/service.did.js
@@ -0,0 +1,34 @@
+export const idlFactory = ({ IDL }) => {
+  const Employee = IDL.Record({
+    'id' : IDL.Text,
+    'isEmployed' : IDL.Bool,
+    'salary' : IDL.Float64,
+    'updatedAt' : IDL.Opt(IDL.Nat64),
+    'department' : IDL.Text,
+    'position' : IDL.Text,
+    'lastName' : IDL.Text,
+    'firstName' : IDL.Text,
+  });
+  const _AzleResult = IDL.Variant({
+    'Ok' : IDL.Opt(Employee),
+    'Err' : IDL.Text,
+  });
+  const _AzleResult_1 = IDL.Variant({ 'Ok' : Employee, 'Err' : IDL.Text });
+  const _AzleResult_2 = IDL.Variant({
+    'Ok' : IDL.Vec(Employee),
+    'Err' : IDL.Text,
+  });
+  return IDL.Service({
+    'deleteEmployee' : IDL.Func([IDL.Text], [_AzleResult], []),
+    'demoteEmployee' : IDL.Func([IDL.Text], [_AzleResult_1], []),
+    'fireEmployee' : IDL.Func([IDL.Text], [_AzleResult_1], []),
+    'getEmployee' : IDL.Func([IDL.Text], [_AzleResult_1], ['query']),
+    'getEmployees' : IDL.Func([], [_AzleResult_2], ['query']),
+    'hireEmployee' : IDL.Func([Employee], [_AzleResult_1], []),
+    'increaseSalary' : IDL.Func([IDL.Text, IDL.Float64], [_AzleResult_1], []),
+    'promoteEmployee' : IDL.Func([IDL.Text], [_AzleResult_1], []),
+    'searchEmployees' : IDL.Func([IDL.Text], [_AzleResult_2], ['query']),
+    'updateEmployee' : IDL.Func([IDL.Text, Employee], [_AzleResult_1], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
